8a03ca814a9a0abe542e690a2915dff34fea11d7
amin2ace <amin.vmap3240@gmail.com>
2025-04-15 23:13:08 +0330

    Import config and mailer module's options

--- apps/mail/src/mail.module.ts
+++ apps/mail/src/mail.module.ts
@@ -1,10 +1,26 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { MailController } from './mail.controller';
 import { MailService } from './mail.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { validationScehema } from './config';
+import { MailerOptionsService } from './mailer.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: 'apps/mail/.env',
+      isGlobal: true,
+      skipProcessEnv: true,
+      validationSchema: validationScehema,
+    }),
+
+    MailerModule.forRootAsync({
+      inject: [ConfigService],
+      useClass: MailerOptionsService,
+    }),
+  ],
   controllers: [MailController],
-  providers: [MailService],
+  providers: [MailService, MailerOptionsService],
 })
 export class MailModule {}
